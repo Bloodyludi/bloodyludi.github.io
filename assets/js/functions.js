e9af56b93fb3b55513104e886a48609f1a31a38b
Ludwig Lauer <contact@llauer.de>
2017-05-03 21:03:16 +0200

    Refactors functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,94 +1,96 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+  init();
+
+  AddListeners();
+
+  show();
+});
+
+$(window).resize(function () {
+  detectScreenSize();
+});
+
+function detectScreenSize() {
+  if (($(window).width() <= 1023)) {
+    var body = $('body');
+    if (!body.hasClass('hide-sidebar')) {
+      body.addClass('hide-sidebar');
+    }
+  }
+  else {
+    var body = $('body');
+    if (body.hasClass('hide-sidebar')) {
+      body.removeClass('hide-sidebar');
+    }
+  }
+}
+
+function init() {
+  $.ajaxSetup({ cache: false });
+
+  detectScreenSize();
+  initFullpage();
+}
+
+function initFullpage() {
   if (selectorExists('#fullpage')) {
     $('#fullpage').fullpage({
-      anchors:['welcome', 'about','work','contact'],
+      anchors: ['welcome', 'about', 'work', 'contact'],
       menu: '#navigation',
       loopBottom: true,
       css3: true,
       resize: false
     });
   }
+}
 
-  $('body').fadeTo(500,1);
-});
+function show() {
+  $('body').fadeTo(500, 1);
+}
 
-$(function() {
-    workLoad();
-    
-    clickBlog();
-    sidebarButtonHandler();
-    detectScreenSize();
-});
+function AddListeners() {
+  $('.thumb-unit').click(function () {
+    var $this = $(this),
+      title = $this.find('.thumb-description').text(),
+      folder = $this.data('folder'),
+      spinner = '<div class="loader">Loading...</div>',
+      newHTML = '/work/' + folder + '.html';
 
-$(window).resize(function() {
-    detectScreenSize();
-});
+    $('.project-load').html(spinner).load(newHTML);
+    $('.project-title').text(title);
+
+    $('.work-slider').addClass("slided");
+    $('.work-container').animateShow(1200);
+    $('.thumb-container').animateShow(1200);
+  });
+
+  $('.work-return').click(function () {
+    $('.work-slider').removeClass("slided");
+    $('.work-container').animateShow(800);
+    $('.thumb-container').animateShow(800);
+  });
+
+  sidebarButtonHandler();
+}
+
+function sidebarButtonHandler() {
+  $(".sidebar-button").click(function (e) {
+    e.preventDefault();
+
+    var body = $('body');
+    if (body.hasClass('hide-sidebar')) {
+      body.removeClass('hide-sidebar');
+    }
+    else {
+      body.addClass('hide-sidebar');
+    }
+  });
+}
 
 function selectorExists(selector) {
   return $(selector).length;
 }
 
-function detectScreenSize() {
-    if (($(window).width() <= 1023)) {
-        var body = $('body');
-        if (!body.hasClass('hide-sidebar'))
-        {
-            body.addClass('hide-sidebar');
-        }
-    } else {
-        var body = $('body');
-        if (body.hasClass('hide-sidebar'))
-        {
-            body.removeClass('hide-sidebar');
-        }
-    }
-}
-
-function clickBlog() {
-    $('.thumb-unit').click(function() {
-        $('.work-slider').addClass("slided");
-        $('.work-container').animateShow(1200);
-        $('.thumb-container').animateShow(1200);
-    });
-    
-    $('.work-return').click(function() {
-        $('.work-slider').removeClass("slided");
-        $('.work-container').animateShow(800);
-        $('.thumb-container').animateShow(800);
-    })
-}
-
-function sidebarButtonHandler() {
-    $(".sidebar-button").click(function(e){
-        e.preventDefault();
-        var body = $('body');
-        if (body.hasClass('hide-sidebar'))
-        {
-            body.removeClass('hide-sidebar');
-        }
-        else
-        {
-            body.addClass('hide-sidebar');
-        }
-    });
-}
-
-$.fn.animateShow = function(duration) {
-  return this.animate({opacity: "toggle"}, duration || 1000);
+$.fn.animateShow = function (duration) {
+  return this.animate({ opacity: "toggle" }, duration || 1000);
 };
-
-function workLoad() {
-    $.ajaxSetup({ cache: false });
-    
-    $('.thumb-unit').click(function() {
-        var $this = $(this),
-            title = $this.find('.thumb-description').text(),
-            folder = $this.data('folder'),
-            spinner = '<div class="loader">Loading...</div>',
-            newHTML = '/work/'+ folder +'.html';
-            
-        $('.project-load').html(spinner).load(newHTML);
-        $('.project-title').text(title);
-        
-    });
-}
